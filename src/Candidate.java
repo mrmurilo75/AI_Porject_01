37c3b3c95b507c9248c800c16665bb9d08da089c
felipe <fvalverde007@gmail.com>
2021-03-23 14:46:31 +0000

    Added a NeighbourList method which keeps all neighbours from the original candidate

--- src/Candidate.java
+++ src/Candidate.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-class Candidate extends ArrayList<Coordinate>{
+class Candidate extends ArrayList<Coordinate> {
     ArrayList<Coordinate> list;
-    IntersectionMap intersected;
+    NeighbourList nbors;
+    public int intersections;
 
-    public Candidate(ArrayList<Coordinate> result){
-	    super(result);
-	    intersected = new IntersectionMap(this);
+    public Candidate(ArrayList<Coordinate> result) {
+        list = new ArrayList<>(result);
+        intersections = 0;
+        nbors = new NeighbourList(this);
     }
 
     /**
@@ -17,50 +19,54 @@ class Candidate extends ArrayList<Coordinate>{
      * @param answer The array in which we wish to put the result in.
      */
     public static ArrayList<Coordinate> randomPermutation(ArrayList<Coordinate> base) {
-	ArrayList<Coordinate> answer = new ArrayList<>();
+        ArrayList<Coordinate> answer = new ArrayList<>();
         int size = base.size();
         while (size != 0) {
             int i = new Random().nextInt(size--);
             answer.add(base.remove(i));
         }
-	return answer;
+        return answer;
     }
 
     /**
-     * Insert into the answer ArrayList the neighbours sorted by who is nearest to the starting point.
-     * @param base The array containing the points we wish to operate on.
+     * Insert into the answer ArrayList the neighbours sorted by who is nearest to
+     * the starting point.
+     * 
+     * @param base   The array containing the points we wish to operate on.
      * @param answer The array in which we wish to put the result in.
      */
     public static ArrayList<Coordinate> nearestNeighbour(ArrayList<Coordinate> base) {
-	ArrayList<Coordinate> answer = new ArrayList<>();
-     
+        ArrayList<Coordinate> answer = new ArrayList<>();
+
         Coordinate cur = base.remove(0);
-        while(cur != null) {
+        while (cur != null) {
             answer.add(cur);
             cur = findNearest(cur, base);
         }
-        return answer; 
+        return answer;
     }
 
     /**
      * Analyses all non-visited neighbours and finds the nearest.
-     * @param cur The current coordinate we are measuring distances from.
+     * 
+     * @param cur  The current coordinate we are measuring distances from.
      * @param base The ArrayList containing its neighbours.
      * @return the nearest coordinate.
      */
     private static Coordinate findNearest(Coordinate cur, ArrayList<Coordinate> base) {
-        if(base.size()==0) return null;
+        if (base.size() == 0)
+            return null;
 
         int toBeRemoved = 0;
         double distance = Double.MAX_VALUE;
         double curX = (double) cur.getX();
         double curY = (double) cur.getY();
 
-        for(int i = 0; i < base.size(); i++) {
+        for (int i = 0; i < base.size(); i++) {
             double nextX = base.get(i).getX();
             double nextY = base.get(i).getY();
             double nextDist = euclidianDistance(curX, curY, nextX, nextY);
-            if(nextDist<distance) {
+            if (nextDist < distance) {
                 distance = nextDist;
                 toBeRemoved = i;
             }
@@ -71,6 +77,7 @@ class Candidate extends ArrayList<Coordinate>{
 
     /**
      * Find the square of the euclidian distance between two points.
+     * 
      * @param x1 X coordinate of the first point.
      * @param y1 Y coordinate of the first point.
      * @param x2 X coordinate of the second point.
@@ -78,36 +85,62 @@ class Candidate extends ArrayList<Coordinate>{
      * @return the square of the euclidian distance.
      */
     private static double euclidianDistance(double x1, double y1, double x2, double y2) {
-        return (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
+        return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
     }
 
-    private void exchange(Coordinate j, Coordinate k){
-	    int ij = this.indexOf(j), 
-		ik = this.indexOf(k);
-	    this.add(ij, k);
-	    this.add(ik, j);
-    }
-
-    /*
-     *
-     */
-    public void improveBestFirst(){
+    private void exchange(Coordinate j, Coordinate k) {
+        int ij = this.indexOf(j), ik = this.indexOf(k);
+        this.add(ij, k);
+        this.add(ik, j);
     }
 
     /*
      *
      */
-    public void improveFirst(){
+    public void improveBestFirst() {
     }
 
     /*
      *
      */
-    public void improveLessConflict(){}
+    public void improveFirst() {
+    }
 
     /*
      *
      */
-    public void improve(){}
+    public void improveLessConflict() {
+    }
 
+    /*
+     *
+     */
+    public void improve() {
+    }
+
+    private void printArrayList(ArrayList<Coordinate> result) {
+        if(result == null){
+		System.out.println("result is null");
+		return;
+	}
+        String ans = "[";
+        for (int i = 0; i < result.size(); i++) {
+            ans += result.get(i).printName();
+            if (i != result.size() - 1)
+                ans += ", ";
+        }
+        ans += "]";
+        System.out.println(ans);
+    }
+
+ 
+
+    public void printNeighbours() {
+        System.out.print("Original order: ");
+        printArrayList(this.list);
+        System.out.println("Number of intersections: " + this.intersections);
+        for(ArrayList<Coordinate> list : nbors) {
+            printArrayList(list);
+        }
+    }
 }
